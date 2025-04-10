7e824633a2e757afa076be7a171f607f9405a585
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2025-04-10 10:20:11 +0200

    add: button to home page

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+const Button = () => {
+    return (
+        <button onClick={() => console.log("is clicked")} style={{color: "black", backgroundColor:"red"}}>
+            Click here
+        </button>
+    )
+}
 
-const ButtonLink: React.FC = () => {
-  return (
-    <Link to="/home" style={{ color: 'black', backgroundColor: 'red', padding: '10px', textDecoration: 'none', display: 'inline-block' }}>
-      Click here
-    </Link>
-  );
-};
-
-export default ButtonLink;
+export default Button;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,10 @@
+import Button from "../../components/Button/Button";
 const HomePage = () => {
     return (
         <div>
             <p>Home page</p>
+            <Button/>
+        
         </div>
     )
 }
